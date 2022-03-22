adb8484af0099b645b64f6a53c41eac8f7eedff6
Azlina <karim.azlina@gmail.com>
2022-03-22 10:05:48 +0800

    init

---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>My List</h1>
+
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Item</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr>
+                                    <td>Makan</td>
+                                    <td>Not Done</td>
+                                    <td><button class="btn btn-primary">Done</button></td>
+                                </tr>
+
+                                <tr>
+                                    <td>Minum</td>
+                                    <td>Not Done</td>
+                                    <td><button class="btn btn-primary">Done</button></td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row text-white">
+            <div class="col bg-primary">Col 1</div>
+            <div class="col bg-success">Col 2</div>
+            <div class="col bg-warning">Col 3</div>
+        </div>
+    </div>
+
+</body>
+
+</html>
